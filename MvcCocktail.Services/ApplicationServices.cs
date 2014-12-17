d31ec991daeb97dce489dd5001fcf56d9bb036e6
simonray <simonray@hotmail.com>
2014-12-17 08:02:53 +0000

    Tidyup

---
+++ MvcCocktail.Services/ApplicationServices.cs
@@ -0,0 +1,15 @@
+﻿using MvcCocktail.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcCocktail.Services
+{
+    public partial class ApplicationServices : IApplicationServices
+    {
+        public static IApplicationServices Instance { get { return new ApplicationServices(); } }
+        private Context NewContext { get { return Context.Instance; } }
+    }
+}
